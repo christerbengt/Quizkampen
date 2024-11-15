480b7095f0dd077ec39718a49cf2912bffdb9078
Björn <bjorn.solman@gmail.com>
2024-11-15 10:51:36 +0100

    testing commeting system one.

--- src/Quizgame/QuestionDatabase.java
+++ src/Quizgame/QuestionDatabase.java
@@ -1,4 +1,29 @@
 package Quizgame;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QuestionDatabase {
+    private List<Question> questions;
+    public QuestionDatabase(String path) {
+        questions = initDataBase(path);
+    }
+
+    private List<Question> initDataBase(String path) {
+        // todo: SKapa textDoc och skapa questions utifrån det!
+        return new ArrayList<>();
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    //
+    //  Function that returns a list of questions at a given topic based on input string.
+    //
+    /////////////////////////////////////////////////////////////////////
+    public List<Question> getQuestionsByCategory(String category) {
+        return questions.stream()
+                .filter(e -> e.getCategory().contains(category))
+                .map(e -> new Question(e.getQuestion(), e.getCategory(), e.getAnswerOption1(), e.getAnswerOption2(), e.getAnswerOption3(), e.getAnswerOption4(), e.getCorrectAnswer()))
+                .collect(Collectors.toList());
+    }
 }
