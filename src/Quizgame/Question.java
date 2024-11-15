5e75a65e35b4dbac5fd7a92c8edf4761f8ddd4a7
Björn <bjorn.solman@gmail.com>
2024-11-15 10:51:36 +0100

    First commit

--- src/Quizgame/Question.java
+++ src/Quizgame/Question.java
@@ -1,4 +1,49 @@
 package Quizgame;
 
 public class Question {
+    private final String question;
+    private final String category;
+    private final String answerOption1;
+    private final String answerOption2;
+    private final String answerOption3;
+    private final String answerOption4;
+    private final String correctAnswer;
+
+    public Question(String question, String category, String answerOption1, String answerOption2, String answerOption3, String answerOption4, String correctAnswer) {
+        this.question = question;
+        this.category = category;
+        this.answerOption1 = answerOption1;
+        this.answerOption2 = answerOption2;
+        this.answerOption3 = answerOption3;
+        this.answerOption4 = answerOption4;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAnswerOption1() {
+        return answerOption1;
+    }
+
+    public String getAnswerOption2() {
+        return answerOption2;
+    }
+
+    public String getAnswerOption3() {
+        return answerOption3;
+    }
+
+    public String getAnswerOption4() {
+        return answerOption4;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
 }
--- src/Quizgame/QuestionDatabase.java
+++ src/Quizgame/QuestionDatabase.java
@@ -1,4 +1,24 @@
 package Quizgame;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QuestionDatabase {
+    private List<Question> questions;
+    public QuestionDatabase(String path) {
+        questions = initDataBase(path);
+    }
+
+    private List<Question> initDataBase(String path) {
+        // todo: SKapa textDoc och skapa questions utifrån det!
+        return new ArrayList<>();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questions.stream().
+                filter(e -> e.getCategory().contains(category))
+                .map(e -> new Question(e.getQuestion(), e.getCategory(), e.getAnswerOption1(), e.getAnswerOption2(), e.getAnswerOption3(), e.getAnswerOption4(), e.getCorrectAnswer()))
+                .collect(Collectors.toList());
+    }
 }
