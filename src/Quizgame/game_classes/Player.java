ff92e73be5206f0f0e498d83b8c5e3d9fdfde60a
ElliotNykv <elliot.nykvist@yh.nackademin.se>
2024-11-18 20:30:47 +0100

    Player class created

--- src/Quizgame/game_classes/Player.java
+++ src/Quizgame/game_classes/Player.java
@@ -1,4 +1,28 @@
 package Quizgame.game_classes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+  private String id;
+  private String name;
+  private List<GameSession> currentGames;
+
+  public Player(String id, String name, List<GameSession> currentGames) {
+    this.id = id;
+    this.name = name;
+    this.currentGames = currentGames;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public List<GameSession> getGameSessions(){
+    return currentGames;
+  }
 }
+
