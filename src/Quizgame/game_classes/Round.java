929ab3e87af19cde699922769f889e5b1ae4caea
ElliotNykv <elliot.nykvist@yh.nackademin.se>
2024-11-19 21:46:12 +0100

    round class created

--- src/Quizgame/game_classes/Round.java
+++ src/Quizgame/game_classes/Round.java
@@ -1,4 +1,41 @@
 package Quizgame.game_classes;
 
+import Quizgame.server_database.Question;
+
+import java.util.List;
+
 public class Round {
+  final private String category;
+  final private List<Question> questionList;
+  // private Map<Player, List<Answer>> playerAnswers;
+  private boolean isComplete;
+
+  public Round(String category, List<Question> questionList) {
+    this.category = category;
+    this.questionList = questionList;
+    this.isComplete = false;
+
+  }
+
+  // public void submitAnswer(Player player, Answer answer) {
+  // }
+
+  public Object getPlayer() {
+    return null;
+  }
+
+
+  public int getScore() {
+    return 0;
+  }
+
+  public boolean isComplete() {
+    return isComplete;
+  }
+
+  //private boolean isCorrect(Answer answer) {
+  //}
+
+
+
 }
