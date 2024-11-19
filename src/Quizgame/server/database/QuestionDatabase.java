46c895e095aabd56eb9464324995052febb31755
bjorn <bjorn.solman@gmail.com>
2024-11-19 09:09:11 +0100

    Questions done. Fill with questions.

--- src/Quizgame/server/database/QuestionDatabase.java
+++ src/Quizgame/server/database/QuestionDatabase.java
@@ -1,18 +1,31 @@
 package Quizgame.server.database;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class QuestionDatabase {
-    private List<Question> questions;
+    private final List<Question> questions;
+
     public QuestionDatabase(String path) {
         questions = initDataBase(path);
     }
 
     private List<Question> initDataBase(String path) {
         // todo: SKapa textDoc och skapa questions utifrån det!
-        return new ArrayList<>();
+        List<Question> q = new ArrayList<>();
+        File f = new File(path);
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] i = line.split(",");
+                q.add(new Question(i[0], i[6], i[3], i[4], i[5], i[6], i[2]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return q;
     }
 
     public List<Question> getQuestionsByCategory(String category) {
---
+++ test/Quizgame/server/database/QuestionDatabaseTest.java
@@ -0,0 +1,18 @@
+package Quizgame.server.database;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuestionDatabaseTest {
+
+    QuestionDatabase database = new QuestionDatabase("files/MusikQuiz_with_Category.csv");
+
+    @Test
+    public void getQuestionsByCategory() {
+        assertEquals(20, database.getQuestionsByCategory("Music").size());
+        for (int i = 0; i < database.getQuestionsByCategory("Music").size(); i++) {
+            System.out.println(database.getQuestionsByCategory("Music").get(i).getQuestion());
+        }
+    }
+}
