2abf5183b88fab8bd3e20a48f9f3be7eae20193b
bjorn <bjorn.solman@gmail.com>
2024-11-20 16:45:28 +0100

    updated with isQuestion correct and refactored questions and initDataBase

--- test/Quizgame/server/database/QuestionDatabaseTest.java
+++ test/Quizgame/server/database/QuestionDatabaseTest.java
@@ -6,13 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionDatabaseTest {
 
-    QuestionDatabase database = new QuestionDatabase("files/MusikQuiz_with_Category.csv");
+    QuestionDatabase database = new QuestionDatabase("files/");
 
     @Test
     public void getQuestionsByCategory() {
-        assertEquals(20, database.getQuestionsByCategory("Music").size());
-        for (int i = 0; i < database.getQuestionsByCategory("Music").size(); i++) {
-            System.out.println(database.getQuestionsByCategory("Music").get(i).getQuestion());
+        assertEquals(20, database.getQuestionsByCategory("Sport").size());
+        for (int i = 0; i < database.getQuestionsByCategory("Sport").size(); i++) {
+            System.out.println(database.getQuestionsByCategory("Sport").get(i).getQuestion());
         }
+    }
+
+    @Test
+    public void isQuestionCorrect(){
+        assertTrue(database.isQuestionCorrect("xxx", "XXX"));
     }
 }
