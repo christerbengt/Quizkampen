1d84d7b8de190149ac57ffefd87889ff475328cc
bSolman <33519000+bSolman@users.noreply.github.com>
2024-11-21 18:43:44 +0100

    Update Round.java

--- src/Quizgame/game_classes/Round.java
+++ src/Quizgame/game_classes/Round.java
@@ -1,51 +1,59 @@
 package Quizgame.game_classes;
 
 import Quizgame.server.database.Question;
+import Quizgame.server.database.QuestionDatabase;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Round {
-  final private String category;
-  final private List<Question> questionList;
-  // private Map<Player, List<Answer>> playerAnswers;
-  private boolean isComplete;
+    final private String category;
+    private final List<Question> questionList;
+    private final LinkedList<Question> randomQuestionList = new LinkedList<>();
+    // private Map<Player, List<Answer>> playerAnswers;
+    private boolean isComplete;
 
-  public Round(String category, List<Question> questionList) {
-    this.category = category;
-    this.questionList = questionList;
-    this.isComplete = false;
-
-  }
-
-  public Object getPlayer() {
-    return null;
-  }
-
-  // public void submitAnswer(Player player, Answer answer) {
-  // }
-
-
-  // public int getRoundScore(Player player) {
-  //  return 0;
-  // }
-
-  public boolean isComplete() {
-    return isComplete;
-  }
-
-  public Question getRandomQuestion() {
-    if (questionList == null || questionList.isEmpty()) {
-      return null;
+    public Round(String category) {
+      this.category = category;
+      this.isComplete = false;
+      this.questionList = getQuestionByTopic(category);
+      createRandomQuestionList();
     }
-    Random random = new Random();
-    int randomIndex = random.nextInt(questionList.size());
-    return questionList.get(randomIndex);
 
-  }
+    private List<Question> getQuestionByTopic(String topic) {
+        QuestionDatabase questionDatabase = new QuestionDatabase("files/");
+        return questionDatabase.getQuestionsByCategory(topic);
+    }
+
+    public Object getPlayer() {
+        return null;
+    }
+
+    // public void submitAnswer(Player player, Answer answer) {
+    // }
 
 
-  public int getScore() {
-    return 0;
-  }
+    // public int getRoundScore(Player player) {
+    //  return 0;
+    // }
+
+    public boolean isComplete() {
+      return isComplete;
+    }
+
+    public void createRandomQuestionList() {
+        Collections.shuffle(questionList);
+        randomQuestionList.addAll(questionList.subList(0, 2));
+    }
+
+    public Question getQuestion(){
+        return randomQuestionList.pop();
+    }
+
+    public LinkedList<Question> getRandomQuestionList() {
+        return randomQuestionList;
+    }
+
+    public int getScore() {
+        return 0;
+    }
 }
