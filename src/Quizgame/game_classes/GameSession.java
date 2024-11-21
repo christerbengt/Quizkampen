70781c8767788e3d46dfe4f6fdd647714dfddd9b
ElliotNykv <elliot.nykvist@yh.nackademin.se>
2024-11-21 17:47:46 +0100

    Method getPoints is added and addPoints also updated getScore

--- src/Quizgame/game_classes/GameSession.java
+++ src/Quizgame/game_classes/GameSession.java
@@ -1,63 +1,45 @@
 package Quizgame.game_classes;
 
-import java.util.List;
-
 public class GameSession {
   private final String sessionId;
   private final Player player1;
   private final Player player2;
-  private final List<Round> rounds;
+  private final int rounds;
   private int currentRoundIndex;
+  private int scorePlayer1 = 0;
+  private int scorePlayer2 = 0;
 
   public GameSession(String sessionId, Player player1, Player player2, int rounds, int currentRoundIndex) {
     this.sessionId = sessionId;
     this.player1 = player1;
     this.player2 = player2;
     this.rounds = rounds;
-    this.currentRoundIndex = 0;
+    this.currentRoundIndex = currentRoundIndex;
   }
 
-  /*
-
-  public void handlePlayerMove(Player, Answer) {
+  public void addPoint(Player player) {
+    if (player.equals(player1)) {
+      scorePlayer1++;
+    } else if (player.equals(player2)) {
+      scorePlayer2++;
+    }
   }
 
-  public GameState getCurrentState() {
-    return currentState;
-  }
-
-  public void setCurrentState(GameState currentState) {
-    this.currentState = currentState;
-  }
-
-  */
   public boolean isRoundComplete() {
-    return currentRoundIndex >= rounds.size();
+    return currentRoundIndex >= rounds;
   }
-
-  public void getScoreBoard() {
-  }
-
-  public Player getPlayer1() {
-    return player1;
-  }
-
-  public Player getPlayer2() {
-    return player2;
-  }
-
-  public String getSessionId() {
-    return sessionId;
-  }
-
 
   public int getScore(Player player) {
-    int score = 0;
-    for (Round round : rounds) {
-      if (round.getPlayer().equals(player)) {
-        score += round.getScore(); // Assuming each Round has a method getScore() and getPlayer()
-      }
+    if (player.equals(player1)) {
+      return scorePlayer1;
+    } else {
+      return scorePlayer2;
     }
-    return score;
+  }
+
+  public String getScoreBoard() {
+      return "Score" + player1.getName() + ":" + scorePlayer1 + "Score" + player1.getName() + ":" + scorePlayer2;
   }
 }
+
+
